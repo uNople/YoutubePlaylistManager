96663a1f4ad51af3bf6b03a749601eb815580e1a
uNople <unople@gmail.com>
2020-11-20 22:29:08 +1300

    Initial commit before I change everything to use google libraries

---
+++ YoutubeCleanupTool/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutubeCleanupTool
+{
+    public static class ExtensionMethods
+    {
+		public static void ForEach<T>(this IEnumerable<T> values, Action<T> action)
+		{
+			foreach (var v in values)
+			{
+				action(v);
+			}
+		}
+	}
+}
---
+++ YoutubeCleanupTool/Program.cs
@@ -0,0 +1,52 @@
+﻿using AdysTech.CredentialManager;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace YoutubeCleanupTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.WaitAll(Execute());
+        }
+        
+        private static async Task Execute()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                // TODO:
+                // Get credentials with name googleapikey
+                // make requests to youtube/youtube music api with key=API_KEY
+                var credential = new NetworkCredential("", CredentialManager.GetICredential("googleapikey").CredentialBlob);
+                var response = await httpClient.GetAsync($"https://www.googleapis.com/youtube/v3/playlists?maxResults=500&prettyPrint=true&channelId=UCsDyfu6em80Ug1T59poKt2Q&key={credential.Password}");
+                var result = await response.Content.ReadAsStringAsync();
+                var parsedResult = JsonConvert.DeserializeObject<JObject>(result);
+
+                var playlistItems = new List<Playlist>();
+                foreach (var item in parsedResult)
+                {
+                    if (item.Key == "items")
+                    {
+                        playlistItems = item.Value.ToObject<List<Playlist>>();
+                    }
+                }
+            }
+        }
+
+        [DebuggerDisplay("{Id}")]
+        class Playlist
+        {
+            public string Kind { get; set; }
+            public string Etag { get; set; }
+            public string Id { get; set; }
+        }
+    }
+}

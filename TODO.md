6565e66524f72cfba06ed6a8eeb357b016607ce1
uNople <unople@gmail.com>
2020-11-26 21:33:32 +1300

    Get playlists using youtube api.
    Oauth working
    I can get all of my playlists, including the private ones

--- TODO.md
+++ TODO.md
@@ -1,4 +1,10 @@
-- Use google nuget packages to do oauth
+- Save the data, so I don't need to get it from the API all the time (cost savings)
+  eg, JSON serialize/deserialize
+  Read it in if it's there, or a 'force' has been passed in
+- Make sure I can get the liked playlist
+- Get the contents of all the playlists (title of playlist, titles of videos in there, etc)
+
+
 - Using that great power, see if I can modify the liked playlist (like a video, unlike it / delete from liked playlist)
 - See if I can add to a playlist
 
--- YoutubeCleanupTool/Program.cs
+++ YoutubeCleanupTool/Program.cs
@@ -1,12 +1,20 @@
 ﻿using AdysTech.CredentialManager;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Security;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace YoutubeCleanupTool
@@ -25,28 +33,41 @@ namespace YoutubeCleanupTool
                 // TODO:
                 // Get credentials with name googleapikey
                 // make requests to youtube/youtube music api with key=API_KEY
-                var credential = new NetworkCredential("", CredentialManager.GetICredential("googleapikey").CredentialBlob);
-                var response = await httpClient.GetAsync($"https://www.googleapis.com/youtube/v3/playlists?maxResults=500&prettyPrint=true&channelId=UCsDyfu6em80Ug1T59poKt2Q&key={credential.Password}");
-                var result = await response.Content.ReadAsStringAsync();
-                var parsedResult = JsonConvert.DeserializeObject<JObject>(result);
-
-                var playlistItems = new List<Playlist>();
-                foreach (var item in parsedResult)
+                var apiKey = CredentialManager.GetICredential("googleapikey").UserName;
+                UserCredential credential;
+                using (var stream = new FileStream(@"C:\Users\unopl\source\repos\Creds\client_secret.json", FileMode.Open, FileAccess.Read))
                 {
-                    if (item.Key == "items")
-                    {
-                        playlistItems = item.Value.ToObject<List<Playlist>>();
-                    }
+                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        new[] { YouTubeService.Scope.YoutubeReadonly },
+                        "user", CancellationToken.None, new FileDataStore("Youtube.Api.Storage"));
                 }
-            }
-        }
 
-        [DebuggerDisplay("{Id}")]
-        class Playlist
-        {
-            public string Kind { get; set; }
-            public string Etag { get; set; }
-            public string Id { get; set; }
+                // Create the service.
+                var service = new YouTubeService(new BaseClientService.Initializer()
+                {
+                    ApiKey = apiKey,
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Youtube cleanup tool",
+                });
+                apiKey = null;
+
+                var playlistRequest = service.Playlists.List("snippet");
+                playlistRequest.Mine = true;
+                // NOTE: it's limited to 50 by google
+                playlistRequest.MaxResults = 50;
+                var playlistResponse = await playlistRequest.ExecuteAsync();
+                var playlists = new List<Playlist>();
+                playlists.AddRange(playlistResponse.Items);
+
+                while (playlistResponse.NextPageToken != null)
+                {
+                    playlistRequest.PageToken = playlistResponse.NextPageToken;
+                    playlistResponse = await playlistRequest.ExecuteAsync();
+                    playlists.AddRange(playlistResponse.Items);
+                }
+
+            }
         }
     }
 }
